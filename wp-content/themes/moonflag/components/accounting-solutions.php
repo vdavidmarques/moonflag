0e0d4816f08eeef3617c0985da66c55d52c762d3
vinicius <vdavidmarques@gmail.com>
2024-11-21 23:30:58 -0300

    chore: Updating accounting-solutions component

--- wp-content/themes/moonflag/components/accounting-solutions.php
+++ wp-content/themes/moonflag/components/accounting-solutions.php
@@ -1,18 +1,22 @@
 <section class="accounting-solutions">
     <?php
-        $title = get_field('title_solutions', $homePageId);
-        $solutions_list = get_field('solutions_list', $homePageId);
-
-        foreach($solutions_list as $key => $item):
-
-        ?>
-        <div class="solution-list">
-            <img src="<?php echo $item['icon']['url']; ?>" alt="<?php echo $item['title']; ?>">
-            <h3><?php echo $item['text']; ?></h3>
-        </div>
-        <?php endforeach; ?>
-
-        <h2><?php echo $title; ?></h2>
-
+    $title = get_field('title_solutions', $homePageId);
+    $solutions_list = get_field('solutions_list', $homePageId);
     ?>
+    <div class="container">
+        <div class="accounting-solutions--header">
+            <h2 class="title"><?php echo $title; ?></h2>
+        </div>
+        <div class="accounting-solutions--lists">
+            <?php foreach ($solutions_list as $key => $item): ?>
+                <div class="accounting-solutions--lists--list">
+                    <img src="<?php echo $item['icon']['url']; ?>" alt="<?php echo $item['title']; ?>" class="image">
+                    <div class="desc">
+                        <?php echo $item['text']; ?></h3>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+    
+        </div>
+    </div>
 </section>
