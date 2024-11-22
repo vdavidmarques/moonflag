a44d82aac2d7ee8cd4405ab52d5501050a30d3e1
vinicius <vdavidmarques@gmail.com>
2024-11-21 22:12:30 -0300

    chore: Updating service component style

--- wp-content/themes/moonflag/components/services.php
+++ wp-content/themes/moonflag/components/services.php
@@ -1,24 +1,39 @@
 <section class="services">
-    <?php     
-        $title = get_field('title_services', $homePageId);
-        $desc = get_field('desc', $homePageId);
-        $button_talk_services = get_field('button_talk_services', $homePageId);
-        $service_lists = get_field('service_lists', $homePageId);
-
-        foreach($service_lists as $key => $value):
-            ?>
-    <div class="service-list">
-            <img src="<?php echo $value['icon']['url']; ?>" alt="<?php echo $value['title']; ?>">
-            <h3><?php echo $value['title']; ?></h3>
-            <p><?php echo $value['link']['url']; ?><?php echo $value['link']['title']; ?></p>
+    <div class="container">
+        <?php
+            $title = get_field('title_services', $homePageId);
+            $desc = get_field('desc', $homePageId);
+            $button_talk_services = get_field('button_talk_services', $homePageId);
+            $service_lists = get_field('service_lists', $homePageId);
+        ?>
+        <div class="services--header">
+            <div class="main-title">
+                <h2 class="title"><?php echo $title; ?></h2>
+            </div>
+            <div class="content">
+                <p class="desc"><?php echo $desc; ?></p>
+                <a href="<?php echo $button_talk_services['url']; ?>" class="link">
+                    <?php echo $button_talk_services['title']; ?>
+                </a>
+            </div>
         </div>
-    <?php endforeach; ?>
 
-        <h2><?php echo $title; ?></h2>
-        <p><?php echo $desc; ?></p>
-        <a href="<?php echo $button_talk_services['url']; ?>">
-            <?php echo $button_talk_services['title']; ?>
-        </a>
-
-    ?>
+        <div class="services--list">
+            <?php foreach ($service_lists as $key => $value): ?>
+                <div class="the-list">
+                    <div class="image">
+                        <img src="<?php echo $value['icon']['url']; ?>" alt="<?php echo $value['title']; ?>">
+                    </div>
+                    <div class="title">
+                        <h3 ><?php echo $value['title']; ?></h3>
+                    </div>
+                    <div class="link">
+                        <a href="<?php echo $value['link']['url']; ?>">
+                            <?php echo $value['link']['title']; ?>
+                        </a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </section>
