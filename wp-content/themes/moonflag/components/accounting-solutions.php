bacc72ed94d8233e9010010bf13c6189864a6ff9
vinicius <vdavidmarques@gmail.com>
2024-11-23 11:21:19 -0300

    chore: Updating accounting-solutions with swiper

--- wp-content/themes/moonflag/components/accounting-solutions.php
+++ wp-content/themes/moonflag/components/accounting-solutions.php
@@ -3,20 +3,24 @@
     $title = get_field('title_solutions', $homePageId);
     $solutions_list = get_field('solutions_list', $homePageId);
     ?>
-    <div class="container">
+    <div class="container accounting-solutions--container"">
         <div class="accounting-solutions--header">
             <h2 class="title"><?php echo $title; ?></h2>
         </div>
-        <div class="accounting-solutions--lists">
-            <?php foreach ($solutions_list as $key => $item): ?>
-                <div class="accounting-solutions--lists--list">
-                    <img src="<?php echo $item['icon']['url']; ?>" alt="<?php echo $item['title']; ?>" class="image">
-                    <div class="desc">
-                        <?php echo $item['text']; ?></h3>
+        <div class="swiper-container">
+            <div class="accounting-solutions--lists swiper-wrapper">
+                <?php foreach ($solutions_list as $key => $item): ?>
+                    <div class="accounting-solutions--lists--list swiper-slide">
+                        <img src="<?php echo $item['icon']['url']; ?>" alt="<?php echo $item['title']; ?>" class="image">
+                        <div class="desc">
+                            <?php echo $item['text']; ?></h3>
+                        </div>
                     </div>
+                    <?php endforeach; ?>
                 </div>
-            <?php endforeach; ?>
-    
+            <div class="swiper-button-prev arrow-swiper arrow-swiper-prev"></div>
+            <div class="swiper-button-next arrow-swiper arrow-swiper-next"></div>
+            <div class="swiper-pagination"></div>
         </div>
     </div>
 </section>
--- wp-content/themes/moonflag/src/js/scripts.js
+++ wp-content/themes/moonflag/src/js/scripts.js
@@ -1,9 +1,65 @@
+const swiperElement = document.querySelector(
+  ".accounting-solutions .swiper-container"
+);
+const swiperWrapper = swiperElement.querySelector(".swiper-wrapper");
+
+const swiperSlides = swiperElement.querySelectorAll(".swiper-slide");
+swiperSlides.forEach((slide, _) => {
+  const clonedSlide = slide.cloneNode(true);
+  swiperWrapper.appendChild(clonedSlide);
+});
+
+const swiper = new Swiper(swiperElement, {
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  autoplay: {
+    delay: 8000,
+  },
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    640: {  
+      slidesPerView: 1.5,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function equalizeHeights() {
+  const slides = document.querySelectorAll(
+    ".accounting-solutions .swiper-slide"
+  );
+
+  let maxHeight = 0;
+  slides.forEach((slide) => {
+    const height = slide.offsetHeight;
+    if (height > maxHeight) maxHeight = height;
+  });
+
+  slides.forEach((slide) => {
+    slide.style.height = `${maxHeight}px`;
+  });
+}
+
+window.addEventListener("load", equalizeHeights);
+window.addEventListener("resize", equalizeHeights);
+
 function scrollTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
 var body = document.querySelector("body");
-
 
 function openMenu() {
   var element = document.querySelectorAll(".menu-items");
@@ -22,12 +78,29 @@ function closeMenu() {
   body.classList.remove("overflow-hidden");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var menuModal = document.querySelector('.menu-items');
-  var menuItems = document.querySelectorAll('.menu-items .menu-item a');
+function openModal(){
+  var modal = document.querySelectorAll(".contact-us-modal");
+  modal.forEach(function(item){
+    item.classList.add("openned");
+  })
+  scrollTop();
+  body.classList.add("overflow-hidden");
+}
 
-  menuItems.forEach(function(item) {
-    item.addEventListener('click', function() {
+function closeModal(){
+  var modal = document.querySelectorAll(".contact-us-modal");
+  modal.forEach(function(item){
+    item.classList.remove("openned");
+  })
+  body.classList.remove("overflow-hidden");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var menuModal = document.querySelector(".menu-items");
+  var menuItems = document.querySelectorAll(".menu-items .menu-item a");
+
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function () {
       menuModal.classList.remove("openned");
       body.classList.remove("overflow-hidden");
     });
@@ -35,7 +108,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // document.addEventListener('DOMContentLoaded', function() {
-  
+
 //   var scrollToTopBtn = document.querySelector('#scrollToTopBtn');
 
 //   window.addEventListener('scroll', function() {
@@ -53,7 +126,6 @@ document.addEventListener('DOMContentLoaded', function() {
 //     });
 //   });
 // });
-
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   const blocks = document.querySelectorAll('.scroll-effect');
@@ -83,29 +155,28 @@ document.addEventListener('DOMContentLoaded', function() {
 //   })
 // }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   function updateCheckboxLinks() {
-      const checkboxes = document.querySelectorAll('.checkbox-offer label span');
+    const checkboxes = document.querySelectorAll(".checkbox-offer label span");
 
-      checkboxes.forEach((checkbox) => {
-          const textContent = checkbox.innerHTML;
+    checkboxes.forEach((checkbox) => {
+      const textContent = checkbox.innerHTML;
 
-          const updatedContent = textContent.replace(
-              /Política de Dados./,
-              '<a href="#politica-de-dados" target="_blank" rel="noopener noreferrer">Política de Dados.</a>'
-          );
+      const updatedContent = textContent.replace(
+        /Política de Dados./,
+        '<a href="#politica-de-dados" target="_blank" rel="noopener noreferrer">Política de Dados.</a>'
+      );
 
-          checkbox.innerHTML = updatedContent;
-      });
+      checkbox.innerHTML = updatedContent;
+    });
   }
 
   updateCheckboxLinks();
 
-  document.addEventListener('wpcf7submit', updateCheckboxLinks);
+  document.addEventListener("wpcf7submit", updateCheckboxLinks);
 
-  document.addEventListener('wpcf7mailsent', updateCheckboxLinks);
-  document.addEventListener('wpcf7invalid', updateCheckboxLinks);
-  document.addEventListener('wpcf7spam', updateCheckboxLinks);
-  document.addEventListener('wpcf7error', updateCheckboxLinks);
+  document.addEventListener("wpcf7mailsent", updateCheckboxLinks);
+  document.addEventListener("wpcf7invalid", updateCheckboxLinks);
+  document.addEventListener("wpcf7spam", updateCheckboxLinks);
+  document.addEventListener("wpcf7error", updateCheckboxLinks);
 });
-
